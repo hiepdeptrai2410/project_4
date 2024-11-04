af3c96124fefae09e18ca72d4f667861e9d7706b
CaoNgocHiep <ngochiep24102001@gmail.com>
2024-11-05 01:20:25 +0700

    Udapte source code

--- starter/backend/src/lambda/auth/auth0Authorizer.ts
+++ starter/backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,7 +1,7 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 require('source-map-support/register')
 import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
+import { createLogger } from '../../utils/logger.mjs'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
